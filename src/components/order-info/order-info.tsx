901d51cb48a0e8cfa10fe6c1f6f4811c1f85480a
MrLargha <cppcodery@gmail.com>
2025-06-20 16:14:37 +0300

    Первая попытка сдачи работы

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,23 +1,24 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useSelector, useDispatch } from '../../services/store';
+import { getOrderByNumber } from '../../services/orderSlice';
 
-export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+interface TOrderInfoProps {
+  title?: boolean;
+}
 
-  const ingredients: TIngredient[] = [];
+export const OrderInfo: FC<TOrderInfoProps> = (props) => {
+  const dispatch = useDispatch();
+  const number = Number(useParams().id);
+  useEffect(() => {
+    dispatch(getOrderByNumber(number));
+  }, [number]);
 
-  /* Готовим данные для отображения */
+  const ingredients = useSelector((state) => state.ingredientData.ingredients);
+  const orderData = useSelector((state) => state.feedsData.testOrderByNumber);
   const orderInfo = useMemo(() => {
     if (!orderData || !ingredients.length) return null;
 
@@ -61,6 +62,10 @@ export const OrderInfo: FC = () => {
 
   if (!orderInfo) {
     return <Preloader />;
+  }
+
+  if (props.title) {
+    return <OrderInfoUI title={`#${number}`} orderInfo={orderInfo} />;
   }
 
   return <OrderInfoUI orderInfo={orderInfo} />;
--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,22 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { Preloader } from '@ui';
+import { useSelector, useDispatch } from '../../services/store';
+import { getUserOrders } from '../../services/userSlice';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const { userOrders: orders, request } = useSelector(
+    (store) => store.userData
+  );
+  useEffect(() => {
+    dispatch(getUserOrders());
+  }, []);
+
+  if (request) {
+    return <Preloader />;
+  }
 
   return <ProfileOrdersUI orders={orders} />;
 };
